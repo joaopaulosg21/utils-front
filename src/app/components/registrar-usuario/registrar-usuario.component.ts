6d7d7bb15caa5083ad0403ec79e4d4e8b876ec27
joaopaulo <joaopaulosg17@gmail.com>
2023-10-06 19:59:16 -0300

    feat: Adicionada pagina para login de usuario

--- src/app/components/registrar-usuario/registrar-usuario.component.ts
+++ src/app/components/registrar-usuario/registrar-usuario.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Route, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 import { CreateUser } from 'src/app/types/CreateUser.interface';
 @Component({
   selector: 'app-registrar-usuario',
@@ -6,8 +9,13 @@ import { CreateUser } from 'src/app/types/CreateUser.interface';
   styleUrls: ['./registrar-usuario.component.css']
 })
 export class RegistrarUsuarioComponent {
-  
+  constructor(private userService:UserService, private snackBar:MatSnackBar,
+    private router:Router) {}
+
   create(user:CreateUser) {
-    console.log(user);
+    this.userService.createUser(user).subscribe(data => {
+      this.snackBar.open("Sua conta foi registrada com sucesso","",{duration:2000});
+      this.router.navigateByUrl("/conta/login");
+    });
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { CreateUser } from '../types/CreateUser.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +9,10 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor(private http:HttpClient) { }
+  private api:string = environment.api;
 
-  
+  createUser(user:CreateUser){
+    return this.http.post("http://localhost:8080/api/users/",user);
+  }
+
 }
