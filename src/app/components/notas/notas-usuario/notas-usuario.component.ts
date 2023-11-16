a91b3d77d0015a7265a719fae07e9dceb0c95bde
joaopaulo <joaopaulosg17@gmail.com>
2023-11-16 10:07:03 -0300

    feat: Mudanças no component de ver notas

--- src/app/components/notas/notas-usuario/notas-usuario.component.ts
+++ src/app/components/notas/notas-usuario/notas-usuario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NotesService } from 'src/app/services/notes.service';
 import { UserService } from 'src/app/services/user.service';
+import { Note } from 'src/app/types/Note.interface';
 
 @Component({
   selector: 'app-notas-usuario',
@@ -14,11 +15,13 @@ export class NotasUsuarioComponent implements OnInit{
     this.findAll();
   }
 
+  notes: Note[] = [];
 
   findAll() {
     this.userService.getToken().subscribe((token:any) => {
       this.notesService.findAll(token).subscribe((data:any) => {
         console.log(data);
+        this.notes = data;
       });
     });
   }
