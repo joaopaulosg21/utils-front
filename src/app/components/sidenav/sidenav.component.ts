dbeadab8e95a3eb507f130f61dd274ecebb0f0b7
joaopaulo <joaopaulosg17@gmail.com>
2024-07-15 11:20:58 -0300

    feat: Adicionada animação para a sidenav

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -4,13 +4,30 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
   imports: [NavbarComponent, MatSidenavModule, MatButtonModule, MatMenuModule,RouterLink,RouterLinkActive],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.css'
+  styleUrl: './sidenav.component.css',
+  animations:[
+    trigger('slideInOut', [
+      state('void', style({
+        transform: 'translateX(-100%)'
+      })),
+      state('*', style({
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate('0.5s ease-in-out')
+      ]),
+      transition('* => void', [
+        animate('0.5s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class SidenavComponent {
   showSideEvent = false;
