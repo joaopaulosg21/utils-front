a21cf7dda4a7b887a87cef7dd88818df7abe679c
joaopaulo <joaopaulosg17@gmail.com>
2024-07-16 11:50:04 -0300

    feat: Adicionada config inicial para o component newlist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AllListsComponent } from './components/list/all-lists/all-lists.component';
+import { NewlistComponent } from './components/list/newlist/newlist.component';
 
 export const routes: Routes = [
-    {component:AllListsComponent,path:"list/all"}
+    {component:AllListsComponent,path:"list/all"},
+    {component:NewlistComponent,path:"list/new"}
 ];
---
+++ src/app/components/list/newlist/newlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-newlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './newlist.component.html',
+  styleUrl: './newlist.component.css'
+})
+export class NewlistComponent {
+
+}
