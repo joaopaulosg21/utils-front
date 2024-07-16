7b88a92cbace0fc6b887981096afffbdb0d86823
joaopaulo <joaopaulosg17@gmail.com>
2024-07-16 17:16:20 -0300

    feat: Adicionados campos para adicionar novas listas, e remover elementos da lista

--- src/app/components/list/newlist/newlist.component.ts
+++ src/app/components/list/newlist/newlist.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { Item } from '../../../../types/Item';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-newlist',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatListModule, MatIconModule],
   templateUrl: './newlist.component.html',
   styleUrl: './newlist.component.css'
 })
 export class NewlistComponent {
+  items: Item[] = [];
 
+  addItemToList(name: string, quantity: string) {
+    const item: Item = {
+      name: name,
+      quantity: quantity
+    }
+
+    if ((!item.quantity.includes("quantidade")) && item.quantity.trim().split(" ")[1] == undefined) {
+      item.quantity = item.quantity + " unidades";
+    }
+
+    if (item.name.length >= 12) {
+      item.name = item.name.substring(0, 12);
+    }
+
+    this.items.push(item);
+  }
+
+  deleteItemFromList(itemName: string) {
+    this.items = this.items.filter(i => i.name != itemName);
+  }
 }
